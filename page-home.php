20d06dbcd95fd19b56c47c8800decdf73bdce79e
Amanda Regan <Amanda@Amandas-iMac.local>
2016-01-13 16:00:37 -0500

    Editing the message below this slider to say changing "in early 2016" rather than "next year" to avoid confusion.

--- page-home.php
+++ page-home.php
@@ -45,7 +45,7 @@ Template Name: Home Page Template
 <div class="container" id="um-notice" >
   <div class="row text-center">
     <div class="col-md-12">
-      <p><i class="fa fa-asterisk"></i> Heads up! Our Editor-at-Large sign up and user registration process is changing next year. <a href="http://dhnow.org/1Tov478"> Read about the forthcoming changes here. </a></p>
+      <p><i class="fa fa-asterisk"></i> Heads up! Our Editor-at-Large sign up and user registration process is changing in early 2016. <a href="http://dhnow.org/1Tov478"> Read about the forthcoming changes here. </a></p>
     </div>
   </div> <!-- close row -->
 </div>
