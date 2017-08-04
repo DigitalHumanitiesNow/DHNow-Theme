e86942a01ff18e0dc8070b56e51a2b5bfd3d001c
Amanda Regan <regan008@cougars.csusm.edu>
2017-08-04 13:19:18 -0400

    add paging to function to create html list of all subscribed feeds.

--- parts/content-feeds.php
+++ parts/content-feeds.php
@@ -6,31 +6,41 @@
 
     <section class="entry-content" itemprop="articleBody">
 <?php
+
       //construct the feeds endpoint url
-      $endpoint_url = site_url($path = 'wp-json/pf/v1/feeds');
-
+      //$endpoint_url = site_url($path = 'wp-json/pf/v1/feeds');
+      $endpoint_url = 'http://www.digitalhumanitiesnow.org/wp-json/pf/v1/feeds?per_page=100';
       //fetch response
-      $response = wp_remote_get( $endpoint_url );
-
+      $response = wp_remote_get($endpoint_url);
       //account for errors
 	    if( is_wp_error( $response ) ) {
 		      return;
 	    }
-      //decode response
-	    $feeds = json_decode( wp_remote_retrieve_body( $response ) );
-	    if( empty( $feeds ) ) {
-		      return;
-	    }
+      if (is_array($response)) {
+        $header = $response['headers'];
+      }
+      $totalpages = $header['x-wp-totalpages'];
+      echo '<script>console.log('.$totalpages.')</script>';
+      for($i = 1; $i <= $totalpages; $i++) {
+        $endpointpage = $endpoint_url . '&page=' . $i;
+        echo '<script>console.log('.$i.')</script>';
+        //echo '<script>console.log('.$endpointpage.')</script>';
+        //echo '<script>console.log("Getting the following url: "'. $endpointpage . '")</script>"';
+        $getpageresponse = wp_remote_get($endpointpage);
+        $feeds = json_decode(wp_remote_retrieve_body($response));
+        foreach( $feeds as $feed ) {
+         $feedlist[] = '<li><a href="' . $feed->feedUrl . '">' . $feed->title->rendered . '</a></li>';
 
-      //construct a list of each feed.
-      if( !empty( $feeds ) ) {
-		      echo '<ul>';
-		    foreach( $feeds as $feed ) {
-			    echo '<li><a href="' . $feed->feedUrl . '">' . $feed->title->rendered . '</a></li>';
-		    }
-		   echo '</ul>';
-	    }
+      }
+    }
 ?>
+<ul>
+<?php
+  foreach($feedlist as $key=>$value) {
+    echo $value;
+  }
+ ?>
+ </ul>
 	</section> <!-- end article section -->
 
 	<footer class="article-footer">
