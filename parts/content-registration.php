ff0f9701e7653780d768d2b9d59e14ac5a6490eb
Mandy Regan <Mandy@MacBook-Pro-51.local>
2017-09-26 10:44:57 -0400

    changes to captcha calls

--- parts/content-registration.php
+++ parts/content-registration.php
@@ -197,10 +197,12 @@
 
 
 
-  <!-- SUBMIT/RESET -->
+
   <div class="g-recaptcha" data-sitekey="6LcEcC8UAAAAAApN8JuKh1GYSEou7OJCpu7PLqF3"></div>
+
     <fieldset class="large-6 columns">
-      <button class="button" type="submit" id="btn-new-user" value="Submit" display="none">Submit</button>
+      <input name="wp-submit" id="btn-new-user" class="button" value="Submit" type="submit">
+      <!-- <button class="button" type="submit" id="btn-new-user" value="Submit" display="none">Submit</button> -->
     </fieldset>
     <fieldset class="large-6 columns">
       <button class="button" type="reset" value="Reset">Reset</button>
