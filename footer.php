2f96b3b33b229abcf39912868389ca1008d5c5d0
Amanda Regan <amanda@Amandas-iMac.local>
2018-09-25 13:16:22 -0400

    style footer images

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer class="footer" role="contentinfo">
 	<div id="inner-footer" class="row">
 		<div class="large-3 medium-12 columns">
-				<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/07/Sloan_Logo_Primary_Web.png"/>
+				<img id="footer-logo" src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/07/Sloan_Logo_Primary_Web.png"/>
 			</div>
-			<div class="large-3 medium-12 columns">
+			<div id="footer-logo" class="large-3 medium-12 columns">
 					<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/05/rrchnm-logo.png"/>
 				</div>
-				<div class="large-3 medium-12 columns pf-logo">
+				<div id="footer-logo" class="large-3 medium-12 columns pf-logo">
 						<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2011/09/pressforward_publication_11.png" />
 					</div>
-					<div class="large-3 medium-12 columns cc-logo">
+					<div id="footer-logo" class="large-3 medium-12 columns cc-logo">
 							<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png"  />
 							<p>This work is licensed under a Creative Commons Attribution 4.0 International License</p>
 						</div>
