e9cf4e79bed06c258bbcd891ba8a5d2ac7c38c77
Amanda Regan <amanda@Amandas-iMac.local>
2018-09-25 13:18:07 -0400

    style footer images

--- footer.php
+++ footer.php
@@ -3,14 +3,14 @@
 		<div class="large-3 medium-12 columns">
 				<img id="footer-logo" src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/07/Sloan_Logo_Primary_Web.png"/>
 			</div>
-			<div id="footer-logo" class="large-3 medium-12 columns">
-					<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/05/rrchnm-logo.png"/>
+			<div class="large-3 medium-12 columns">
+					<img id="footer-logo" src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/05/rrchnm-logo.png"/>
 				</div>
-				<div id="footer-logo" class="large-3 medium-12 columns pf-logo">
-						<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2011/09/pressforward_publication_11.png" />
+				<div class="large-3 medium-12 columns pf-logo">
+						<img id="footer-logo" src="http://digitalhumanitiesnow.org/wp-content/uploads/2011/09/pressforward_publication_11.png" />
 					</div>
-					<div id="footer-logo" class="large-3 medium-12 columns cc-logo">
-							<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png"  />
+					<div  class="large-3 medium-12 columns cc-logo">
+							<img id="footer-logo" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png"  />
 							<p>This work is licensed under a Creative Commons Attribution 4.0 International License</p>
 						</div>
 				</div>  <!-- end .main-content -->
