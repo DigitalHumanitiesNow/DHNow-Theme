ba343ccb3473cfd45b5324dee32c824f3c2c0fa2
Amanda Regan <amanda@Amandas-iMac.local>
2018-09-25 12:46:51 -0400

    fix footer image size

--- footer.php
+++ footer.php
@@ -7,10 +7,10 @@
 					<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2015/05/rrchnm-logo.png"/>
 				</div>
 				<div class="large-3 medium-12 columns">
-						<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2011/09/pressforward_publication_11.png"/>
+						<img src="http://digitalhumanitiesnow.org/wp-content/uploads/2011/09/pressforward_publication_11.png" height=200px/>
 					</div>
 					<div class="large-3 medium-12 columns">
-							<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png"/>
+							<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png" height=200px />
 							<p>This work is licensed under a Creative Commons Attribution 4.0 International License</p>
 						</div>
 				</div>  <!-- end .main-content -->
